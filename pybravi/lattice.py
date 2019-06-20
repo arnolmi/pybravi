40080383432628c437dcb461df7863872051e224
Michael Arnold <nim@shadowfire.org>
2019-06-20 20:13:01 +1000

    Add a radial slicer

--- pybravi/lattice.py
+++ pybravi/lattice.py
@@ -4,7 +4,7 @@ module for generating bravais lattices.
 import numpy as np
 
 
-def gen_lattice_vector(vector, angle):
+def create_lattice_vector(vector, angle):
     """
     Generates the component of a lattice vector given an angle.
 
@@ -20,13 +20,14 @@ def gen_lattice_vector(vector, angle):
     return b_vec, a_vec
 
 
-def gen_lattice(shape, lattice_vectors):
+def create_lattice(shape, lattice_vectors, slicer=None):
     """
     Generates a lattice given a shape and lattice vectors
 
     Args:
        shape (tuple): shape of the lattice in a grid of points
        lattice_vectors (np.array): a pair of lattice vectors
+       slicer (func): takes a function that knows how to remove points from  the lattice.
     """
     # Translate along one vector to fill up the bottom row.
     x_pts = np.arange(0, shape[0])
@@ -36,4 +37,29 @@ def gen_lattice(shape, lattice_vectors):
     # now copy this array N times
     points = np.array([x*np.array(lattice_vectors[1]) +
                        base for x in range(0, shape[1])]).reshape(shape[0]*shape[1], 2)
+
+    if slicer is not None:
+        points = slicer(points)
+
     return points
+
+
+def radial_slicer(radius, center=np.array([0, 0])):
+    """
+    Wrapper for methods using the center point
+
+    Args:
+        radius (float): a float representing how far from the center to cut points
+        center (np.array): the center from which we should slice.
+    """
+    def func(points):
+        """
+        Function to remove points outside of a radius from the center
+
+        Args:
+            points (np.array): an array of points
+        """
+        res = [point for point in points if np.linalg.norm(point-center) <= radius]
+        return np.array(res)
+
+    return func
--- tests/test_pybravi.py
+++ tests/test_pybravi.py
@@ -4,12 +4,12 @@
 """Tests for `pybravi` package."""
 
 
-from pybravi.lattice import gen_lattice_vector, gen_lattice
+from pybravi.lattice import create_lattice_vector, create_lattice, radial_slicer
 import numpy as np
 
 
-def test_gen_lattice_vector():
-    vec_a, vec_b = gen_lattice_vector([10, 0], -120)
+def test_create_lattice_vector():
+    vec_a, vec_b = create_lattice_vector([10, 0], -120)
     assert not np.array_equal(vec_a, vec_b)
 
     rads = np.arccos(vec_a.dot(vec_b) / (np.linalg.norm(vec_a) * np.linalg.norm(vec_b)))
@@ -17,12 +17,22 @@ def test_gen_lattice_vector():
     assert np.abs(rads - expected_rads) <= 0.001
 
 
-def test_gen_lattice():
-    vecs = gen_lattice_vector([10, 0], -120)
-    points = gen_lattice((2, 2), vecs)
+def test_create_lattice():
+    vecs = create_lattice_vector([10, 0], -120)
+    points = create_lattice((2, 2), vecs)
 
     # Everything should be roughly 10 from the origin
     for point in points:
         norm = np.linalg.norm(point - np.array([0, 0]))
         if norm != 0:
             assert np.abs(norm - 10 <= 0.001)
+
+
+def test_radial_slicer():
+    func = radial_slicer(radius=1)
+    test_points = np.array([[0, 0], [1, 0], [2, 0]])
+    res = func(test_points)
+    for point in res:
+        assert not np.array_equal(point, np.array([2, 0]))
+        assert np.array_equal(point, np.array([1, 0])) or \
+            np.array_equal(point, np.array([0, 0]))
